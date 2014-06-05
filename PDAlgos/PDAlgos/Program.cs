d03e2c1d9f1273232838887f275c319ccd517576
founderchiu <qiufangda@gmail.com>
2014-06-04 23:20:55 -0500

    yye

--- PDAlgos/PDAlgos/Program.cs
+++ PDAlgos/PDAlgos/Program.cs
@@ -11,6 +11,7 @@ namespace PDAlgos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello PD Algos");
+            Console.WriteLine("Hello Risong");
         }
     }
 }
